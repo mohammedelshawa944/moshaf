b9764dd92dc5738d4dae7374505696679150439f
mohammedshawwa944 <mohammedelshawa944@gmail.com>
2023-07-27 14:10:10 +0300

    first commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -52,7 +52,7 @@ class _HomePageState extends State<HomePage> {
       ),
       drawer: Drawer(
         child:ListView.builder(itemBuilder: (context, index)=> ListTile(
-          
+
         ))
       ),
       body: pages == null ?
